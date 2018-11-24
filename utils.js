618fae60a5d05ebaf52f66d23821d9aad33a943b
Pankaj Vaishnav <pankajvaishnav7792@gmail.com>
2018-11-24 09:44:30 +0530

    commited

--- utils.js
+++ utils.js
@@ -1,4 +1,35 @@
 var fs = require('fs')
+var match=fs.readFileSync('matches.csv', 'utf8');
+var rowArrOfMatch = match.split('\n');
+var matches=[];
+for(var eachRow=0;eachRow<rowArrOfMatch.length;eachRow++){
+  matches[eachRow]=rowArrOfMatch[eachRow].split(',');
+}
+function getIdOfAnySeason(season){
+var arrOfMatchId = [];
+for(let eachRow=1;eachRow<matches.length;eachRow++){
+   if(matches[eachRow][1] === season){
+       arrOfMatchId.push(matches[eachRow][0]);
+   }
+}
+return arrOfMatchId
+}
+module.exports = function() {
+   this.getIdOfAnySeason=function(ofSeason){
+       return getIdOfAnySeason(ofSeason)
+
+   }
+}
+
+
+
+
+
+
+
+
+
+/*var fs = require('fs')
 var csv = require('fast-csv')
 var season = '2016'
 
@@ -20,4 +51,4 @@ var matchIdsPerSeason = function(season){
 matchIdsPerSeason(season);
 module.exports ={
    matchIdsPerSeason: matchIdsPerSeason
-}
+}*/
