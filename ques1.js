469c804a06797e29ab702ed0756ac5a4c323f74c
Pankaj Vaishnav <pankajvaishnav7792@gmail.com>
2018-11-24 11:31:57 +0530

    Final Commit

--- ques1.js
+++ ques1.js
@@ -1,16 +1,11 @@
 var fs = require('fs')
 var csv = require('fast-csv')
 var matchesStream = fs.createReadStream("matches.csv");
-
 var matchesPerSeason = {};
-
-console.time("ques1")
 
 csv
  .fromStream(matchesStream, {headers : true})
  .on("data", function(match){
-     //console.log('I am one line of data', data);
-     //console.log(match)
      if (matchesPerSeason[match['season']] !== undefined){
          matchesPerSeason[match['season']] += 1
      }
@@ -20,7 +15,5 @@ csv
  })
  .on("end", function(){
      console.log(matchesPerSeason);
-     console.timeEnd("ques1")
  });
 
- console.log("Line 22: " + matchesPerSeason)
